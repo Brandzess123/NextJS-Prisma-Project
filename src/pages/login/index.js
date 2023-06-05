bb23bfa19e49c784ee5faedca4d4b367b65de7d9
brandzess123 <lolikimngok@gmail.com>
2023-06-05 09:26:57 +0700

    fix img -> Image in all folder

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import { useSession, signIn, signOut, getSession } from "next-auth/react";
 import { sign } from "crypto";
+import Image from "next/image";
 
 export default function LoginSite() {
   const [pass, setPassword] = useState("");
@@ -122,7 +123,8 @@ export default function LoginSite() {
           */}
       <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
+          <Image
+            layout="fill"
             className="w-auto h-10 mx-auto"
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
             alt="Your Company"
