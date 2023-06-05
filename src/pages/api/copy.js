fba85b56347371b7928dfc356c2b5c2e95a19bd4
brandzess123 <lolikimngok@gmail.com>
2023-06-05 08:58:15 +0700

    fix copy.js

--- src/pages/api/copy.js
+++ src/pages/api/copy.js
@@ -1,49 +1,49 @@
-import { prisma } from "../../server/db/client";
-import { getSession } from "next-auth/client";
-import { authOptions } from "./auth/[...nextauth]";
+// import { prisma } from "../../server/db/";
+// import { getSession } from "next-auth/client";
+// import { authOptions } from "./auth/[...nextauth]";
 
-function titleFromCode(code) {
-  return code.trim().split("\n")[0].replace("// ", "");
-}
+// function titleFromCode(code) {
+//   return code.trim().split("\n")[0].replace("// ", "");
+// }
 
-async function post(req, res) {
-  const session = await getSession({ req });
-  if (!session) {
-    res.status(401).json({ error: "Unauthorized" });
-    return;
-  }
+// async function post(req, res) {
+//   const session = await getSession({ req });
+//   if (!session) {
+//     res.status(401).json({ error: "Unauthorized" });
+//     return;
+//   }
 
-  const prismaUser = await prisma.user.findUnique({
-    where: { email: session.user.email },
-  });
+//   const prismaUser = await prisma.user.findUnique({
+//     where: { email: session.user.email },
+//   });
 
-  if (!prismaUser) {
-    res.status(401).json({ error: "Unauthorized" });
-    return;
-  }
+//   if (!prismaUser) {
+//     res.status(401).json({ error: "Unauthorized" });
+//     return;
+//   }
 
-  const { language, code } = req.body;
-  const title = titleFromCode(code);
-  const post = await prisma.post.create({
-    data: {
-      title,
-      language,
-      code,
-      userId: prismaUser.id,
-    },
-  });
-  res.status(201).json(post);
-}
+//   const { language, code } = req.body;
+//   const title = titleFromCode(code);
+//   const post = await prisma.post.create({
+//     data: {
+//       title,
+//       language,
+//       code,
+//       userId: prismaUser.id,
+//     },
+//   });
+//   res.status(201).json(post);
+// }
 
-export default async function handler(req, res) {
-  const { method } = req;
+// export default async function handler(req, res) {
+//   const { method } = req;
 
-  switch (method) {
-    case "POST":
-      await post(req, res);
-      break;
-    default:
-      res.setHeader("Allow", ["POST"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
-  }
-}
+//   switch (method) {
+//     case "POST":
+//       await post(req, res);
+//       break;
+//     default:
+//       res.setHeader("Allow", ["POST"]);
+//       res.status(405).end(`Method ${method} Not Allowed`);
+//   }
+// }
