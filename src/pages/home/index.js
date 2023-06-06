d0e96235932a0d524eb95f20770537e0b30a715e
brandzess123 <lolikimngok@gmail.com>
2023-06-06 15:02:02 +0700

    route to /home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -144,7 +144,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Example() {
+export default function Home() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   useEffect(() => {
